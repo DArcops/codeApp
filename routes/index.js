30ac62ca51594e49737fe8af56c67e99b3b5905d
diego_lopez_arcos <diego.lopez.arcos@gmail.com>
2016-05-15 18:01:14 -0500

    layout , vista y rutas del register, falta mongodb

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,14 @@ router.get('/login',function(req,res){
   res.render('login/index');
 });
 
+router.get('/login/test',function(req,res){
+  res.render('login/test');
+});
+
+router.post('/welcome',function(req,res){
+  console.log(req.body);
+  var user_name = req.body.user_name;
+  res.render('user/index',{nombre:user_name});
+});
+
 module.exports = router;
