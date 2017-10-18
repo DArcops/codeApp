4f75340e51564aea83ce6ef62383fa5e9007f2f5
darcops <diego.lopez.arcos@gmail.com>
2017-10-18 12:51:37 -0500

    just send post to create lessons

--- public/javascripts/controllers/new_lesson.js
+++ public/javascripts/controllers/new_lesson.js
@@ -1,29 +1,42 @@
 
 angular.module('app', [])
   .controller('New_Lesson',  function($scope, $http, $window) {
-    console.log($('#summernote'));
-    // $(document).ready(function() {
-    //   $('#summernote').summernote();
-    // });
-    $scope.submit = function(){
-      var url = "http://localhost:8088/api/v1/users/login"
-      var data = {
-        "email" : $scope.email,
-        "pass" : $scope.pass,
+
+    if(localStorage.getItem("token") === null)
+      $window.location.href = "/login"
+
+
+    var config = {
+      headers: {
+          'Authorization': localStorage.getItem("token")
       }
-      console.log(data);
-      var config = {};
+    };
+    $http.get("http://localhost:8088/api/v1/courses/",config)
+      .then(function(response) {
+        $scope.courses = response.data;
+        console.log(response.data);
+    });
 
-      $http.post(url, data, config)
-              .success(function (data, status, headers, config) {
-                console.log(data)
-
-                localStorage.setItem("token",data.token)
-                $window.location.href = "/dashboard/courses"
-              })
-              .error(function (data, status, header, config) {
-                console.log(status)
-              });
+    $scope.submit = function(){
+      // var url = "http://localhost:8088/api/v1/users/login"
+      // var data = {
+      //   "email" : $scope.email,
+      //   "pass" : $scope.pass,
+      // }
+      // console.log(data);
+      // var config = {};
+      //
+      // $http.post(url, data, config)
+      //         .success(function (data, status, headers, config) {
+      //           console.log(data)
+      //
+      //           localStorage.setItem("token",data.token)
+      //           $window.location.href = "/dashboard/courses"
+      //         })
+      //         .error(function (data, status, header, config) {
+      //           console.log(status)
+      //         });
+      console.log($scope.selectedCourse)
     }
 
   });
