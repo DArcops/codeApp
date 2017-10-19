6107c4f9910604038bac9a32055187113e3c8aec
darcops <diego.lopez.arcos@gmail.com>
2017-10-19 15:14:12 -0500

    with new struct in database

--- public/javascripts/controllers/new_lesson.js
+++ public/javascripts/controllers/new_lesson.js
@@ -17,13 +17,22 @@ angular.module('app', [])
           'Authorization': localStorage.getItem("token")
       }
     };
+
     $http.get("http://localhost:8088/api/v1/courses/",config)
       .then(function(response) {
         $scope.courses = response.data;
         console.log(response.data);
     });
 
-  
+    $scope.change = function() {
+      $http.get("http://localhost:8088/api/v1/levels?course_id="+$scope.selectedCourse,config)
+        .then(function(response) {
+          $scope.levels = response.data;
+          console.log(response.data);
+      });
+    };
+
+
     $scope.submit = function(){
       // var url = "http://localhost:8088/api/v1/users/login"
       // var data = {
